4d8c3302e48e005af1da096162a4343e18e678d8
Qarib2004 <garibalisultanov@gmail.com>
2024-12-08 11:07:04 +0400

    changes api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,10 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://mica-grape-scabiosa.glitch.me/";
 
 const endpoints = {
   events: "/events",
   tickets: "/tickets",
   users: "/users",
+  venues: "/venues"
 };
 
 export { endpoints };
