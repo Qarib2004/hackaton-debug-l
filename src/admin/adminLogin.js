9b51bf11b934e392e0947dbcffadb6296fd5bae2
jfrlysona <jfrlysona@gmail.com>
2024-12-08 17:19:47 +0400

    admin panel updated

--- src/admin/adminLogin.js
+++
@@ -1,4 +0,0 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const btn = document.getElementById("id");
-const form = document.getElementById("loginForm");
